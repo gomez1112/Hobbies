be02c979996d864834d7b385f4f52603ee0dc396
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2023-11-05 14:25:45 -0500

    Initial Commit

---
+++ Hobbies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hobbies
+//
+//  Created by Gerard Gomez on 11/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hobbies/HobbiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobbiesApp.swift
+//  Hobbies
+//
+//  Created by Gerard Gomez on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct HobbiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
